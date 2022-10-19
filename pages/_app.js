d7633d5b8ae846061e06b12a1402db0591f95fe5
aakrut <aakrutdabhi@gmail.com>
2022-10-19 10:58:20 +0530

    add bundlr-toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { ToastContainer } from "react-toastify";
+import BundlrContextProvider from "../context/bundlrContext";
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <BundlrContextProvider>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </BundlrContextProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
