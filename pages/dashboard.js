fd67ac335463fec495715a32712deaa008b12580
aakrut <aakrutdabhi@gmail.com>
2022-10-20 16:06:21 +0530

    fix changes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,5 @@
 import { gql, useApolloClient } from "@apollo/client";
 import Head from "next/head";
-import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
 
 import React, { useEffect, useRef, useState } from "react";
 import { truncateEthAddress } from "../utils/truncAddress";
@@ -17,7 +15,6 @@ const FETCH_SONGS = gql`
       song
       songName
       genre
-      songArtist
       createdAt
       releaseDate
     }
@@ -33,17 +30,17 @@ const Dashboard = () => {
 
   const toggle = () => setPlaying(!playing);
 
-  const myRef = useRef();
+  const myRef = useRef(null);
 
   const startAudio = () => {
-    myRef.current.play();
-
+    console.log(myRef.current);
+    myRef?.current?.play();
     setPlaying(true);
   };
 
   const pauseAudio = () => {
     console.log("here");
-    myRef.current.pause();
+    myRef?.current?.pause();
     setPlaying(false);
   };
 
@@ -68,6 +65,14 @@ const Dashboard = () => {
       });
   };
 
+  const handlePlay = () => {
+    if (playing) {
+      pauseAudio();
+    } else {
+      startAudio();
+    }
+  };
+
   useEffect(() => {
     getSongs();
   }, [songs]);
@@ -88,12 +93,16 @@ const Dashboard = () => {
           </h1>
           <img src="/logo.png" alt="logo" className="w-[50px] h-[50px]" />
         </div>
-        <div className="max-w-[1240px] mx-auto my-0 grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 p-3 sm:p-5">
+        <div className="max-w-[1240px] mx-auto my-0 grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 p-3 sm:p-5 gap-2">
           {songs?.songs?.length > 0 &&
             songs.songs.map((data) => (
               <div
                 key={data.id}
-                className="border border-solid border-sky-800 rounded-xl p-3 sm:p-5"
+                className="border border-solid border-sky-800 rounded-xl p-3 sm:p-5 cursor-pointer"
+                onClick={() => {
+                  setCurrentSong(data.song);
+                  handlePlay();
+                }}
               >
                 <div className="w-full h-[320px] rounded-lg relative">
                   <img
@@ -109,44 +118,11 @@ const Dashboard = () => {
                     </p>
                   </div>
                 </div>
-                <h3 className="text-2xl my-2">{data.songName}</h3>
-                <div className="flex items-center justify-between">
-                  <h4 className="text-slate-500">
-                    Artist:{" "}
-                    <span className="font-semibold  text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-800">
-                      {truncateEthAddress(data.songArtist)}
-                    </span>
-                  </h4>
-                  <div
-                    className="bg-sky-800 rounded-full p-3 cursor-pointer"
-                    onClick={() => {
-                      setCurrentSong(data.song);
-                    }}
-                  >
-                    {" "}
-                    {!playing ? (
-                      <Play
-                        size="32"
-                        color="#d9e3f0"
-                        variant="Bold"
-                        onClick={() => {
-                          startAudio();
-                          toggle();
-                        }}
-                      />
-                    ) : (
-                      <Pause
-                        size="32"
-                        color="#d9e3f0"
-                        variant="Bold"
-                        onClick={() => {
-                          pauseAudio();
-                          toggle();
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
+                <h3 className="text-2xl my-2">
+                  {data?.songName?.length > 18
+                    ? data?.songName?.slice(0, 18) + "..."
+                    : data?.songName}
+                </h3>
               </div>
             ))}
         </div>
