4ada953b98dd054b752b9c8cd4e1c53375f13d8e
aakrut <aakrutdabhi@gmail.com>
2022-10-21 12:25:14 +0530

    add new layout

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -7,11 +7,12 @@ import {
 } from "iconsax-react";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/AudioPlayer.module.css";
+import { truncateEthAddress } from "../utils/truncAddress";
 
 const mainURL = `https://arweave.net/`;
 
 const AudioPlayer = ({ currentSong }) => {
-    console.log(currentSong)
+  console.log(currentSong);
   const [isPlaying, setIsPlaying] = useState(false);
 
   const [duration, setDuration] = useState(0);
@@ -25,7 +26,11 @@ const AudioPlayer = ({ currentSong }) => {
     const seconds = Math.floor(audioPlayer.current.duration);
     setDuration(seconds);
     progressBar.current.max = seconds;
-  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+  }, [
+    audioPlayer?.current?.loadedmetadata,
+    audioPlayer?.current?.readyState,
+    currentSong,
+  ]);
 
   const calculateTime = (secs) => {
     const minutes = Math.floor(secs / 60);
@@ -85,30 +90,41 @@ const AudioPlayer = ({ currentSong }) => {
       >
         {" "}
       </audio>
-      <button onClick={backwardTen}>
-        <Backward10Seconds size="32" color="#d9e3f0" />
-      </button>
-      <button onClick={togglePlayPause}>
-        {isPlaying ? (
-          <Pause size="32" color="#d9e3f0" variant="Bold" />
-        ) : (
-          <Play size="32" color="#d9e3f0" variant="Bold" />
-        )}
-      </button>
-      <button onClick={forwardTen}>
-        <Forward10Seconds size="32" color="#d9e3f0" />
-      </button>
+      <div>
+        <img src={mainURL + currentSong.image} alt={currentSong.songName} />
+        <h4>{currentSong.songName}</h4>
+      </div>
+      <div>
+        <button onClick={backwardTen}>
+          <Backward10Seconds size="32" color="#d9e3f0" />
+        </button>
+        <button
+          onClick={togglePlayPause}
+          className="bg-blue-700 p-3 rounded-full"
+        >
+          {isPlaying ? (
+            <Pause size="32" color="#d9e3f0" variant="Bold" />
+          ) : (
+            <Play size="32" color="#d9e3f0" variant="Bold" />
+          )}
+        </button>
+        <button onClick={forwardTen}>
+          <Forward10Seconds size="32" color="#d9e3f0" />
+        </button>
+      </div>
 
-      <div>{calculateTime(currentTime)}</div>
+      <div>
+        <div>{calculateTime(currentTime)}</div>
 
-      <input
-        type="range"
-        defaultValue={"0"}
-        ref={progressBar}
-        onChange={changeRange}
-      />
+        <input
+          type="range"
+          defaultValue={"0"}
+          ref={progressBar}
+          onChange={changeRange}
+        />
 
-      <div>{duration && !isNaN(duration) && calculateTime(duration)}</div>
+        <div>{duration && !isNaN(duration) && calculateTime(duration)}</div>
+      </div>
     </div>
   );
 };
